abbe5b559f3b1dd320106d6a56a5c48f4043115b
Kirk <kaesunico@mymail.mapua.edu.ph>
2026-09-24 23:51:40 +0800

    ui changes

--- app.py
+++ app.py
@@ -2,6 +2,60 @@ import streamlit as st
 import pickle
 import random
 
+st.markdown("""
+<style>
+    /* Main app background */
+    .stApp {
+        background-color: #000000;
+    }
+    
+    /* Force all text to be neon green and monospace */
+    h1, h2, h3, p, label, div, span, li {
+        color: #00FF00 !important;
+        font-family: 'Courier New', Courier, monospace !important;
+    }
+    
+    /* Style buttons like terminal commands */
+    .stButton>button {
+        background-color: #000000 !important;
+        color: #00FF00 !important;
+        border: 2px solid #00FF00 !important;
+        border-radius: 0px !important;
+        font-weight: bold;
+        text-transform: uppercase;
+    }
+    .stButton>button:hover {
+        background-color: #00FF00 !important;
+        color: #000000 !important;
+    }
+    
+    /* Style the dropdown boxes */
+    div[data-baseweb="select"] > div {
+        background-color: #000000 !important;
+        border: 1px solid #00FF00 !important;
+        border-radius: 0px !important;
+        color: #00FF00 !important;
+    }
+    
+    /* Style the dropdown list items */
+    ul[data-baseweb="menu"] {
+        background-color: #000000 !important;
+        border: 1px solid #00FF00 !important;
+    }
+    li[data-baseweb="menu-item"] {
+        color: #00FF00 !important;
+    }
+    
+    /* Make dividers look like dashed terminal lines */
+    hr {
+        border-top: 1px dashed #00FF00 !important;
+        margin-top: 2rem;
+        margin-bottom: 2rem;
+    }
+</style>
+""", unsafe_allow_html=True)
+
+
 @st.cache_data
 def load_data():
     with open('movies_list.pkl', 'rb') as f:
@@ -30,25 +84,35 @@ if 'collab_user' not in st.session_state:
 if 'user_dropdown' not in st.session_state:
     st.session_state.user_dropdown = valid_users[0]
 
-st.title("Movie Recommendation Engine")
+st.text("""
+===================================================
+  __  __            _        ____                
+ |  \/  | _____   _(_) ___  |  _ \ ___  ___ ___  
+ | |\/| |/ _ \ \ / / |/ _ \ | |_) / _ \/ __/ __| 
+ | |  | | (_) \ V /| |  __/ |  _ <  __/ (__\__ \ 
+ |_|  |_|\___/ \_/ |_|\___| |_| \_\___|\___|___/ 
+                                                 
+   R E C O M M E N D A T I O N   E N G I N E     
+===================================================
+""")
 
-st.header("Content-Based Filtering")
+st.header("> CONTENT_BASED.EXE")
 st.write("Find similar movies based on genre and metadata.")
 
 selected_movie = st.selectbox("Search for a movie you personally like:", movie_titles)
 
-if st.button("Get Content Recommendations"):
+if st.button("RUN CONTENT_SEARCH"):
     st.session_state.content_results = content_recs.get(selected_movie, [])
     st.session_state.content_movie = selected_movie
 
 if st.session_state.content_results:
     st.write(f"Because you liked **{st.session_state.content_movie}**, based on the model we recommend:")
     for title in st.session_state.content_results:
-        st.write(f"- {title}")
+        st.write(f"  [+] {title}")
 
 st.divider()
 
-st.header("Collaborative Filtering")
+st.header("> COLLAB_FILTER.EXE")
 st.write("Discover different movies based on other user's favorite movies.")
 
 def pick_random_user():
@@ -59,16 +123,16 @@ selected_user = st.selectbox("Select a User ID:", valid_users, key="user_dropdow
 col1, col2 = st.columns(2)
 
 with col1:
-    if st.button("Get Collaborative Recommendations"):
+    if st.button("RUN COLLAB_SEARCH"):
         user_predictions = predictions_df.loc[selected_user].sort_values(ascending=False)
         seen_movies = user_history.get(selected_user, [])
         st.session_state.collab_results = user_predictions.drop(seen_movies, errors='ignore').head(5).index.tolist()
         st.session_state.collab_user = selected_user
 
 with col2:
-    st.button("Random User", on_click=pick_random_user)
+    st.button("RANDOM USER ID", on_click=pick_random_user)
 
 if st.session_state.collab_results:
     st.write(f"Recommendations from User **{st.session_state.collab_user}**:")
     for title in st.session_state.collab_results:
-        st.write(f"- {title}")
+        st.write(f"  [+] {title}")
